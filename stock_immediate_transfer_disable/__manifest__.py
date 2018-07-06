5368680102dcacf9c3948db1cd868b8c2746ad52
David Dufresne <dufresne.david.2@gmail.com>
2018-07-06 14:52:10 -0400

    [11.0] Add module stock_immediate_transfer_disable

--- stock_immediate_transfer_disable/__manifest__.py
+++ stock_immediate_transfer_disable/__manifest__.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
 # © 2017 Savoir-faire Linux
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Disable Immediate Transfer',
-    'version': '10.0.1.0.0',
-    'author': 'Savoir-faire Linux',
-    'maintainer': 'Savoir-faire Linux',
-    'website': 'http://www.savoirfairelinux.com',
+    'version': '11.0.1.0.0',
+    'author': 'Savoir-faire Linux,Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
     'license': 'LGPL-3',
     'category': 'Warehouse',
     'summary': 'Disable Immediate Tranfer on Stock Pickings',
