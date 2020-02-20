3fe57c3cad122d5a448239b46fda8bf76ea32493
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-02-20 12:19:20 -0500

    TA#18149 Add module stock_location_position_alphanum (#20)
    * Add module stock_location_position_alphanum
    
    * Update stock_location.py

---
+++ stock_location_position_alphanum/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2020 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Stock Location Position Alphanum",
+    "summary": "Allow alphanumeric in stock location positions",
+    "version": "1.0.0",
+    "category": "Inventory",
+    "author": "Numigi",
+    "license": "LGPL-3",
+    "depends": [
+        "stock",
+    ],
+    'data': [
+        "views/stock_location.xml",
+    ],
+    "installable": True,
+}
---
+++ stock_location_position_alphanum/models/stock_location.py
@@ -0,0 +1,17 @@
+# © 2020 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class StockLocation(models.Model):
+
+    _inherit = 'stock.location'
+
+    posx = fields.Integer(string='Corridor (X) (Integer)', groups='base.group_no_one')
+    posy = fields.Integer(string='Shelves (Y) (Integer)', groups='base.group_no_one')
+    posz = fields.Integer(string='Height (Z) (Integer)', groups='base.group_no_one')
+
+    posx_alphanum = fields.Char('Corridor (X)')
+    posy_alphanum = fields.Char('Shelves (Y)')
+    posz_alphanum = fields.Char('Height (Z)')
