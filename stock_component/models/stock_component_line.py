62f907c437cc95f6bc0f4afa8488547008428b0b
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-03-27 11:31:08 -0400

    Update stock_component/models/stock_component_line.py
    Co-Authored-By: Jordi Riera <547282+foutoucour@users.noreply.github.com>

--- stock_component/models/stock_component_line.py
+++ stock_component/models/stock_component_line.py
@@ -2,7 +2,6 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import api, fields, models
-from odoo.exceptions import ValidationError
 
 
 class StockComponentLine(models.Model):
