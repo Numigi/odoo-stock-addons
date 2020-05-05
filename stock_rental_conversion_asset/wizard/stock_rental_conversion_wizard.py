b973a730c60026dedc966cb627b8882cb38f8518
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-05-05 16:55:32 -0400

    Codacy

--- stock_rental_conversion_asset/wizard/stock_rental_conversion_wizard.py
+++ stock_rental_conversion_asset/wizard/stock_rental_conversion_wizard.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from datetime import datetime
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 
 
 class StockRentalConversionWizard(models.TransientModel):
