e17342a70156072837a77c6ce8246e966061366b
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-05-15 07:57:51 -0400

    TA#22111 Add module stock_product_location_info

---
+++ stock_product_location_info/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Stock Product Location Info",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Product",
+    "summary": "Add a field Usual Location of products",
+    "depends": ["stock"],
+    "installable": True,
+    "data": ["views/product_template.xml"],
+}
