5e445e33162651a13e376861d65609013bfce74b
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-05-15 10:22:28 -0400

    TA#22111 Add module stock_product_location_info (#33)
    * TA#22111 Add module stock_product_location_info

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -2,22 +2,22 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Main Module',
-    'version': '1.0.0',
-    'author': 'Numigi',
-    'maintainer': 'Numigi',
-    'website': 'https://www.numigi.com',
-    'license': 'LGPL-3',
-    'category': 'Other',
-    'summary': 'Install all addons required for testing.',
-    'depends': [
-        'sale_stock',  # Required to test stock_previous_step_return
-
-        'stock_immediate_transfer_disable',
-        'stock_move_list_cost',
-        'stock_move_origin_link',
-        'stock_picking_show_address',
-        'stock_previous_step_return',
+    "name": "Main Module",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "LGPL-3",
+    "category": "Other",
+    "summary": "Install all addons required for testing.",
+    "depends": [
+        "sale_stock",  # Required to test stock_previous_step_return
+        "stock_immediate_transfer_disable",
+        "stock_move_list_cost",
+        "stock_move_origin_link",
+        "stock_picking_show_address",
+        "stock_previous_step_return",
+        "stock_product_location_info",
     ],
-    'installable': True,
+    "installable": True,
 }
