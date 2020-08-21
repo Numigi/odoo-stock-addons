a0b2daf7f400a39e7a4b5bab0c1b97239584b26f
cuong.nmtm <cuong.nmtm@komit-consulting.com>
2020-08-21 19:57:51 +0700

    T#006590 - #25798 - NB - Ajout des numéros de série dans la recherche de transfert [stock_picking_search_by_serial]

---
+++ stock_picking_search_by_serial/README.rst
@@ -0,0 +1,18 @@
+Stock Picking Search By Serial
+==============================
+- Add the ability to search Transfers by Lot / Serial Number (Barcode)
+
+.. image:: static/description/stock_picking_search_by_serial.png
+
+Configuration
+-------------
+No configuration required apart from module installation.
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+* Komit (https://komit-consulting.com)
+
+More information
+----------------
+* Meet us at https://bit.ly/numigi-com
---
+++ stock_picking_search_by_serial/tests/test_stock_picking_search_by_serial.py
@@ -0,0 +1,51 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo.tests.common import SavepointCase
+
+
+class TestStockPickingSearchBySerial(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        stock_location = cls.env.ref("stock.stock_location_stock")
+        customer_location = cls.env.ref("stock.stock_location_customers")
+        product = cls.env["product.product"].create(
+            {"name": "Product A", "type": "product", "barcode": "RightBarcode"}
+        )
+        stock_picking = cls.env["stock.picking"].create(
+            {
+                "location_id": stock_location.id,
+                "location_dest_id": customer_location.id,
+                "picking_type_id": cls.env.ref("stock.picking_type_out").id,
+            }
+        )
+        cls.env["stock.move"].create(
+            {
+                "name": "stock move",
+                "location_id": stock_location.id,
+                "location_dest_id": customer_location.id,
+                "product_id": product.id,
+                "product_uom": cls.env.ref("uom.product_uom_dozen").id,
+                "product_uom_qty": 1.0,
+                "picking_id": stock_picking.id,
+            }
+        )
+
+    def _check_search_stock_picking_by_barcode(self, barcode, exist_result):
+        res = self.env["stock.picking"].search(
+            [("move_ids_without_package.product_id.barcode", "=", barcode)]
+        )
+        if exist_result:
+            self.assertEqual(len(res), 1)
+        else:
+            self.assertEqual(len(res), 0)
+
+    def test_search_stock_picking_by_barcode_with_result(self):
+        self._check_search_stock_picking_by_barcode(
+            barcode="RightBarcode", exist_result=True
+        )
+
+    def test_search_stock_picking_by_barcode_without_result(self):
+        self._check_search_stock_picking_by_barcode(
+            barcode="RightBar", exist_result=False
+        )
