e7c9369e20ebb997ee82a66a9943cb37cd547882
cuong.nmtm <cuong.nmtm@komit-consulting.com>
2020-08-31 12:53:17 +0700

    Update domain search and automatic tests

--- stock_picking_search_by_serial/tests/test_stock_picking_search_by_serial.py
+++ stock_picking_search_by_serial/tests/test_stock_picking_search_by_serial.py
@@ -16,41 +16,78 @@ class TestStockPickingSearchBySerial(SavepointCase):
                 "uom_id": cls.env.ref("uom.product_uom_unit").id,
             }
         )
-        stock_picking = cls.env["stock.picking"].create(
-            {
-                "location_id": stock_location.id,
-                "location_dest_id": customer_location.id,
-                "picking_type_id": cls.env.ref("stock.picking_type_out").id,
-            }
+        stock_pickings = cls.env["stock.picking"].create(
+            [
+                {
+                    "location_id": stock_location.id,
+                    "location_dest_id": customer_location.id,
+                    "picking_type_id": cls.env.ref("stock.picking_type_out").id,
+                },
+                {
+                    "location_id": stock_location.id,
+                    "location_dest_id": customer_location.id,
+                    "picking_type_id": cls.env.ref("stock.picking_type_out").id,
+                },
+            ]
         )
-        stock_move = cls.env["stock.move"].create(
-            {
-                "name": "stock move",
-                "location_id": stock_location.id,
-                "location_dest_id": customer_location.id,
-                "product_id": product.id,
-                "product_uom": cls.env.ref("uom.product_uom_unit").id,
-                "product_uom_qty": 1.0,
-                "picking_id": stock_picking.id,
-            }
+        stock_moves = cls.env["stock.move"].create(
+            [
+                {
+                    "name": "stock move",
+                    "location_id": stock_location.id,
+                    "location_dest_id": customer_location.id,
+                    "product_id": product.id,
+                    "product_uom": cls.env.ref("uom.product_uom_unit").id,
+                    "product_uom_qty": 1.0,
+                    "picking_id": stock_pickings[0].id,
+                },
+                {
+                    "name": "stock move",
+                    "location_id": stock_location.id,
+                    "location_dest_id": customer_location.id,
+                    "product_id": product.id,
+                    "product_uom": cls.env.ref("uom.product_uom_unit").id,
+                    "product_uom_qty": 1.0,
+                    "picking_id": stock_pickings[1].id,
+                },
+            ]
+        )
+        lot_1 = cls.env["stock.production.lot"].create(
+            {"name": "Right Lot 1", "product_id": product.id}
         )
         cls.env["stock.move.line"].create(
-            {
-                "lot_name": "Right Lot",
-                "move_id": stock_move.id,
-                "product_id": product.id,
-                "product_uom_id": cls.env.ref("uom.product_uom_unit").id,
-                "location_id": stock_location.id,
-                "location_dest_id": customer_location.id,
-            }
+            [
+                {
+                    "lot_id": lot_1.id,
+                    "move_id": stock_moves[0].id,
+                    "picking_id": stock_moves[0].picking_id.id,
+                    "product_id": product.id,
+                    "product_uom_id": cls.env.ref("uom.product_uom_unit").id,
+                    "location_id": stock_location.id,
+                    "location_dest_id": customer_location.id,
+                },
+                {
+                    "lot_name": "Right Lot 2",
+                    "move_id": stock_moves[1].id,
+                    "picking_id": stock_moves[1].picking_id.id,
+                    "product_id": product.id,
+                    "product_uom_id": cls.env.ref("uom.product_uom_unit").id,
+                    "location_id": stock_location.id,
+                    "location_dest_id": customer_location.id,
+                },
+            ]
         )
 
     def _check_search_stock_picking_by_serial(self, serial, exist_result):
         res = self.env["stock.picking"].search(
-            [("move_ids_without_package.move_line_ids.lot_name", "=", serial)]
+            [
+                "|",
+                ("move_line_ids.lot_name", "ilike", serial),
+                ("move_line_ids.lot_id", "ilike", serial),
+            ]
         )
         if exist_result:
-            self.assertEqual(len(res), 1)
+            self.assertEqual(len(res), 2)
         else:
             self.assertEqual(len(res), 0)
 
