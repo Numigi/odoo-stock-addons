490eda2fd2bd8e2d9506761dcb576b04f1c4a898
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-07-07 16:51:36 -0400

    TA#37761 add module stock_move_list_location

---
+++ stock_move_list_location/__manifest__.py
@@ -0,0 +1,14 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Stock Move List Locations",
+    "version": "1.0.0",
+    "category": "Stock",
+    "description": "Add the source and destination locations to the list of stock moves",
+    "maintainer": "numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "depends": ["stock"],
+    "data": ["views/stock_move.xml"],
+}
