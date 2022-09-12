65a6743d648bf50169cac9e2e386774e76e076d4
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2022-09-12 11:25:53 +0100

    Init repository (#65)

--- stock_picking_digitized_signature/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import res_partner, stock_picking, stock_picking_type
