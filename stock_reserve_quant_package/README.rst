7bb35e634d6dc0c609a036500dbcdc3560249f3d
majouda <majdaelmariouli@gmail.com>
2022-10-11 22:28:57 +0100

    TA#47365 Prevent the partial reservation of a package

---
+++ stock_reserve_quant_package/README.rst
@@ -0,0 +1,12 @@
+Stock Reserve Quant Package
+===========================
+
+.. contents:: Table of Contents
+
+Context
+-------
+
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
---
+++ stock_reserve_quant_package/__manifest__.py
@@ -0,0 +1,14 @@
+# © 2022 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Stock Reserve Quant Package",
+    "summary": "Force Quant reservation by package",
+    "version": "1.0.0",
+    "category": "Inventory",
+    "author": "Numigi",
+    "license": "LGPL-3",
+    "depends": ['stock'],
+    "data": [],
+    "installable": True,
+}
