03786a877395c1c7e6d1bcadbb8b5cbf860610cf
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-02-28 02:35:33 +0300

    TA#48326 [REF] Formatting line too long

--- stock_quant_package_product_packaging_dimension/models/stock_quant_package.py
+++ stock_quant_package_product_packaging_dimension/models/stock_quant_package.py
@@ -8,19 +8,40 @@ from odoo import fields, models
 class StockQuantPackage(models.Model):
     """Dimension for stock quant package."""
 
-    _inherit = 'stock.quant.package'
+    _inherit = "stock.quant.package"
 
     def _get_uom_domain(self):
         uom_ids = []
-        uom_ids.extend((self.env.ref('uom.product_uom_foot').id, self.env.ref('uom.product_uom_meter').id))
-        return [('id', 'in', uom_ids)]
+        uom_ids.extend(
+            (
+                self.env.ref("uom.product_uom_foot").id,
+                self.env.ref("uom.product_uom_meter").id,
+            )
+        )
+        return [("id", "in", uom_ids)]
 
     def _get_default_uom(self):
-        return self.env.ref('uom.product_uom_meter')
+        return self.env.ref("uom.product_uom_meter")
 
-    height = fields.Float('Height')
-    width = fields.Float('Width')
-    packaging_length = fields.Float('Length')
-    height_uom_id = fields.Many2one('uom.uom', string='Height UOM', domain=_get_uom_domain, default=_get_default_uom)
-    width_uom_id = fields.Many2one('uom.uom', string='Width UOM', domain=_get_uom_domain, default=_get_default_uom)
-    length_uom_id = fields.Many2one('uom.uom', string='Length UOM', domain=_get_uom_domain, default=_get_default_uom)
+    height = fields.Float("Height")
+    width = fields.Float("Width")
+    packaging_length = fields.Float("Length")
+    height_uom_id = fields.Many2one(
+        "uom.uom",
+        string="Height UOM",
+        domain=_get_uom_domain,
+        default=_get_default_uom
+    )
+    width_uom_id = fields.Many2one(
+        "uom.uom",
+        string="Width UOM",
+        domain=_get_uom_domain,
+        default=_get_default_uom
+    )
+    length_uom_id = fields.Many2one(
+        "uom.uom",
+        string="Length UOM",
+        domain=_get_uom_domain,
+        default=_get_default_uom
+    )
+
