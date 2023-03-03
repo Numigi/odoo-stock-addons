d054f584710b956e14fffa5091612bc3d26b5742
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-03-03 10:51:58 +0300

    TA#48326 [ADD][IMP] Change module name and moving module to stock addons

---
+++ product_packaging_dimension_decimal/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2023 - Today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0).
+
+{
+    'name': 'Product Packaging Dimension Decimal and Changeable UOM',
+    'version': "1.0.0",
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'AGPL-3',
+    'category': 'Stock',
+    'summary': 'Add changeable UOM and allow decimal on product packaging.',
+    'depends': [
+        'delivery',
+    ],
+    'data': [
+        'views/product_packaging.xml',
+    ],
+    'installable': True,
+}
