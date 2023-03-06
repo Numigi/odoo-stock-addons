13d8675fef3ae2ff9b57121ea3d8d51b9ce9b94f
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-03-06 18:46:35 +0300

    TA#48326 [FIX] stock_renta : restore the deleted module

---
+++ stock_rental/models/stock_location.py
@@ -0,0 +1,18 @@
+# © 2022 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class StockLocation(models.Model):
+
+    _inherit = "stock.location"
+
+    is_rental_customer_location = fields.Boolean(
+        help="Check this box if this location is used for products "
+        "rented to a customer."
+    )
+
+    is_rental_stock_location = fields.Boolean(
+        help="Check this box if this location is used for stocks of rental products."
+    )
