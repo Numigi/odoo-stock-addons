a7c0a7d43bc09caace1e84e5fe2065929b1c2d23
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-03-20 17:11:28 +0300

    TA#50339 [FIX] stock_change_qty_reason_enhanced : set default field value to false

---
+++ stock_change_qty_reason_enhanced/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Stock Change Quantity Reason Enhanced',
+    'version': '14.0.1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    "license": "LGPL-3",
+    'category': 'Stock',
+    'summary': 'Fix the install error on the OCA Module stock_change_qty_reason, setting a default value on ``group quantity reason preset`` field.',
+    'depends': [
+        'stock_change_qty_reason',
+    ],
+    'installable': True,
+}
---
+++ stock_change_qty_reason_enhanced/models/res_config_settings.py
@@ -0,0 +1,10 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    group_qty_reason_preset = fields.Boolean(default=False)
