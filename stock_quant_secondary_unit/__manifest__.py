82f97dfdcd4461fd653daeef2358402a18fdc9ed
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2023-08-01 10:09:39 -0400

    Bump Module Version

--- stock_quant_secondary_unit/__manifest__.py
+++ stock_quant_secondary_unit/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Stock Quant Secondary Unit",
-    "version": "1.0.1",
+    "version": "1.0.2",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
