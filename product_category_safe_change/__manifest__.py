138d65db095a7bacc54b683d64804943ce788d53
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-09-28 14:57:40 +0300

    Updating version

--- product_category_safe_change/__manifest__.py
+++ product_category_safe_change/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Category Safe Change",
-    "version": "1.0.1",
+    "version": "1.0.2",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
