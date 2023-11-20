143dbb92ce31292c7fca17cfcce6659595daf41f
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-11-20 18:09:45 +0300

    TA#58439 [ADD][FIX] stock_quant_secondary_unit : new features

--- stock_quant_secondary_unit/README.rst
+++ stock_quant_secondary_unit/README.rst
@@ -19,6 +19,33 @@ This module allows, for an user in the group `Inventory > Manager`, to see the q
 
 .. image:: static/description/location_available_stock.png
 
+Since the version 1.0.3 :
+
+As an Inventory user, I go to the form view of an item/variant and click on either of the stock smart buttons.
+
+* I notice that a new column `Available 2nd Unit` is present, between the `2nd Unit On Hand Qty` field and the `2nd unit` field.
+
+.. image:: static/description/available_2nd_unit_on_stock_quant.png
+
+* I also see that the "Value" field is now after the "2nd unit" field.
+
+.. image:: static/description/value_moved_on_stock_quant.png
+
+* When I click on the 3 small dots, I see that I can choose whether or not to display all the fields available in the view.
+
+.. image:: static/description/optional_show_stock_quant.png
+
+As a member of the `Inventory / Administrator` group, I go to `Inventory > Reporting > Inventory Report`.
+
+* I notice that a new column `Available 2nd Unit`  is present, between the `2nd Unit On Hand Qty` field and the `2nd unit`  field.
+I also see that the `Value` field is now after the `2nd unit` field.
+
+.. image:: static/description/inventory_report_available_2nd_unit.png
+
+* When I click on the 3 small dots, I see that I can choose whether or not to display all the fields available in the view.
+
+.. image:: static/description/inventory_report_optional_show.png
+
 Contributors
 ------------
 * Numigi (tm) and all its contributors (https://bit.ly/numigiens)
--- stock_quant_secondary_unit/models/stock_quant.py
+++ stock_quant_secondary_unit/models/stock_quant.py
@@ -1,6 +1,8 @@
 # © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from lxml import etree
+
 from odoo import api, fields, models
 from odoo.tools.float_utils import float_round
 import logging
@@ -24,6 +26,13 @@ class StockQuant(models.Model):
         store=True,
     )
 
+    available_second_unit = fields.Float(
+        string="Available 2nd Unit",
+        compute="compute_available_second_unit",
+        digits="Product Unit of Measure",
+        store=True,
+    )
+
     @api.depends(
         "stock_secondary_uom_id", "inventory_quantity", "product_uom_id.rounding"
     )
@@ -41,3 +50,42 @@ class StockQuant(models.Model):
                     qty, precision_rounding=stock_quant_line.product_uom_id.rounding
                 )
                 stock_quant_line.write({"secondary_unit_qty_available": val})
+
+    @api.depends("stock_secondary_uom_id", "available_quantity")
+    def compute_available_second_unit(self):
+        if self.user_has_groups("stock.group_stock_manager"):
+            self = self.with_context(inventory_mode=True)
+        for stock_quant_line in self:
+            if not stock_quant_line.stock_secondary_uom_id:
+                stock_quant_line.available_second_unit = 0.0
+            else:
+                qty = stock_quant_line.available_quantity / (
+                    stock_quant_line.stock_secondary_uom_id.factor or 1.0
+                )
+                val = float_round(
+                    qty, precision_rounding=stock_quant_line.product_uom_id.rounding
+                )
+                stock_quant_line.write({"available_second_unit": val})
+
+    @api.model
+    def fields_view_get(
+        self, view_id=None, view_type="form", toolbar=False, submenu=False
+    ):
+        res = super(StockQuant, self).fields_view_get(
+            view_id, view_type, toolbar=toolbar, submenu=False
+        )
+        if view_type != "tree":
+            return res
+
+        if view_type == "tree":
+            tree_view_id = self.env["ir.model.data"].xmlid_to_res_id(
+                "stock.view_stock_quant_tree_editable"
+            )
+            if res.get("view_id") == tree_view_id:
+                doc = etree.XML(res["arch"])
+                for field in res["fields"]:
+                    for node in doc.xpath("//field[@name='%s']" % field):
+                        node.set("optional", "show")
+                res["arch"] = etree.tostring(doc, encoding="unicode")
+
+                return res
