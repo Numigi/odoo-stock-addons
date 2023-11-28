d6b84a6d2f5a0d21c2e9ce39d8a5a9de0f3441bd
majouda <majdaelmariouli@gmail.com>
2023-11-28 18:34:20 -0500

    TA#58439 [IMP] stock_quant_secondary_unit : new features

--- stock_quant_secondary_unit/tests/test_stock_quant_secondary_unit.py
+++ stock_quant_secondary_unit/tests/test_stock_quant_secondary_unit.py
@@ -1,8 +1,6 @@
 # © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from lxml import etree
-
 from odoo.addons.stock_secondary_unit.tests.test_stock_secondary_unit import (
     TestProductSecondaryUnit,
 )
@@ -13,9 +11,9 @@ class TestCustomerReference(TestProductSecondaryUnit):
     def setUpClass(cls):
         super().setUpClass()
 
-    def test_stock_quant_secondary_unit_qty_available(self):
+    def test_stock_quant_inventory_secondary_unit_qty(self):
         # with the first factor 0.5
-        self.assertEqual(self.quant_white.secondary_unit_qty_available, 20.0)
+        self.assertEqual(self.quant_white.inventory_secondary_unit_qty, 20.0)
         self.assertEqual(
             self.quant_white.stock_secondary_uom_id,
             self.quant_white.product_id.stock_secondary_uom_id,
@@ -39,18 +37,5 @@ class TestCustomerReference(TestProductSecondaryUnit):
             white_product.secondary_uom_ids[1],
         )
         self.assertEqual(self.quant_white.stock_secondary_uom_id.factor, 0.9)
-        self.assertEqual(self.quant_white.secondary_unit_qty_available, 11.11)
-        self.assertEqual(self.quant_white.available_second_unit, 11.11)
-
-    def test_fields_view_get_optional_show(self):
-        res = self.env["stock.quant"].fields_view_get(
-            view_id=self.env.ref("stock.view_stock_quant_tree_editable").id,
-            view_type="tree",
-        )
-        eview = etree.fromstring(res["arch"])
-        for field in res["fields"]:
-            for node in eview.xpath("//field[@name='%s']" % field):
-                self.assertEqual(
-                    "show",
-                    node.get("optional", "show"),
-                )
+        self.assertEqual(self.quant_white.inventory_secondary_unit_qty, 11.11)
+        self.assertEqual(self.quant_white.available_secondary_unit_qty, 11.11)
