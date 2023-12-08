fe41f67c189b32ae294221b106082501d1ae306b
majouda <majdaelmariouli@gmail.com>
2023-12-08 16:21:31 -0500

    TA#56785 [ADD] stock_routes_product_multico

---
+++ stock_routes_product_multico/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Routes Product Multico",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "AGPL-3",
+    "category": "Stock",
+    "depends": ["stock"],
+    "summary": "Fix stock multi-company and multi-routes on product.",
+    "data": [],
+    "installable": True,
+}
---
+++ stock_routes_product_multico/models/product.py
@@ -0,0 +1,10 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    route_from_categ_ids = fields.Many2many(related_sudo=False)
