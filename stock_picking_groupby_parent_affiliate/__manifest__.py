e26b6d3b283efee3532331ea0c58b528f39a71af
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-12-14 13:05:04 +0300

    TA#58434 [ADD][IMP] stock_picking_groupby_parent_affiliate : new module and dockerfile updated

---
+++ stock_picking_groupby_parent_affiliate/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Picking group by Parent Affiliate",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "AGPL-3",
+    "category": "Stock",
+    "depends": ["stock", "partner_affiliate_extended"],
+    "summary": "Add the possibility to group by parent affiliate on stock picking.",
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+}
