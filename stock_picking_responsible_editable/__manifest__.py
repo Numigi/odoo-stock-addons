3a2241d08e541cb2ae1fbc536987177203f0ba33
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-12-14 16:17:35 +0300

    TA#59943 [ADD] stock_picking_responsible_editable : new module

---
+++ stock_picking_responsible_editable/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Picking Responsible Editable",
+    "version": "14.0.1.0.1",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "AGPL-3",
+    "category": "Stock",
+    "depends": ["stock"],
+    "summary": "Allow to enter the `Responsible` when the transfer status `out` or `int`  = `Done`.",
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+}
