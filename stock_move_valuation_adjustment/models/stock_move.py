1029ce773b9fc24a9d2ccccf735624778d3dd146
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-12-21 12:51:43 +0300

    TA#60314 [MIG] stock_move_valuation_adjustment : migrated module

---
+++ stock_move_valuation_adjustment/models/stock_move.py
@@ -0,0 +1,20 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models, fields, api
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    valuation_adjustment_line_ids = fields.One2many(
+        "stock.valuation.adjustment.lines", "move_id"
+    )
+    value = fields.Float(string="Value", compute="_compute_total_value")
+
+    @api.depends("stock_valuation_layer_ids")
+    def _compute_total_value(self):
+        for stock_move in self:
+            stock_move.value = sum(
+                [(-svl.value) for svl in stock_move.stock_valuation_layer_ids]
+            )
