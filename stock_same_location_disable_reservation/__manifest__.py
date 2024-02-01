6a8a7b43abd2cc6fd281905ef90c4f30a438af20
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2024-02-01 17:06:35 +0300

    TA#56787 [ADD] stock_same_location_disable_reservation :  quant location check on reservation

--- stock_same_location_disable_reservation/__manifest__.py
+++ stock_same_location_disable_reservation/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Stock Same Location Disable Reservation",
-    "version": "1.0.1",
+    "version": "1.0.2",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
