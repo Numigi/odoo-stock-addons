6f04703eeac2bb82cce300f7876c611106ee0722
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2024-03-27 14:09:43 +0300

    Enabling module on main

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -16,7 +16,7 @@
         "product_packaging_dimension_decimal",
         "stock_account_visibility",
         "stock_auto_assign_disabled",
-        # "stock_auto_assign_disabled_jit",
+        "stock_auto_assign_disabled_jit",
         "stock_change_qty_reason_enhanced",
         "stock_immediate_transfer_disable",
         "stock_location_dest_width",
