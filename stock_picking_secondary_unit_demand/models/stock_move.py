f15ffca30e5b82f5a583e7c2b86ce67e07c5c373
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2024-05-18 05:52:16 +0300

    TA#61866 [UPD] stock_picking_secondary_unit_demand : demand on second uom

--- stock_picking_secondary_unit_demand/models/stock_move.py
+++ stock_picking_secondary_unit_demand/models/stock_move.py
@@ -3,6 +3,7 @@
 
 
 from odoo import api, fields, models
+from odoo.tools.float_utils import float_round
 from odoo.tools.misc import formatLang
 
 
@@ -10,15 +11,42 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     merged_qty_uom_info = fields.Char(
-        string="Request Secondary Unit", readonly=True,
-        compute="_compute_merged_qty_uom_info"
+        string="Request Secondary Unit",
+        readonly=True,
+        compute="_compute_merged_qty_uom_info",
     )
 
-    @api.depends("sale_line_id.secondary_uom_qty", "sale_line_id.secondary_uom_id.name")
+    @api.depends(
+        "product_id",
+        "product_uom",
+        "product_uom_qty",
+        "sale_line_id.secondary_uom_qty",
+        "sale_line_id.secondary_uom_id.name",
+    )
     def _compute_merged_qty_uom_info(self):
         for line in self:
             line.merged_qty_uom_info = "%s %s" % (
                 formatLang(
-                    self.env, line.sale_line_id.secondary_uom_qty),
-                line.sale_line_id.secondary_uom_id.name or ''
+                    self.env,
+                    line.sale_line_id.secondary_uom_qty
+                    or line._get_product_inventory_in_secondary_uom(),
+                ),
+                line.sale_line_id.secondary_uom_id.name
+                or line.product_id.stock_secondary_uom_id.name
+                or "",
             )
+
+    def _get_product_inventory_in_secondary_uom(self):
+        """
+        Get a quantity to the secondary unit of measure.
+        """
+        self.ensure_one()
+        demand_qty = 0.0
+        if self.product_id.stock_secondary_uom_id:
+            demand_qty = self.product_uom_qty / (
+                self.product_id.stock_secondary_uom_id.factor or 1.0
+            )
+            demand_qty = float_round(
+                demand_qty, precision_rounding=self.product_uom.rounding
+            )
+        return demand_qty
