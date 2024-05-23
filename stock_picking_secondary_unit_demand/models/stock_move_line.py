676e2a6ed9dfd15517d6c840cb3dcf7764673677
majouda <majdaelmariouli@gmail.com>
2024-05-23 16:42:53 -0400

    Code Review

--- stock_picking_secondary_unit_demand/models/stock_move_line.py
+++ stock_picking_secondary_unit_demand/models/stock_move_line.py
@@ -2,26 +2,14 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 
-from odoo import api, fields, models
-from odoo.tools.misc import formatLang
+from odoo import fields, models
 
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
     merged_qty_uom_info = fields.Char(
-        string="Request Secondary Unit", readonly=True,
-        compute="_compute_merged_qty_uom_info"
+        string="Request Secondary Unit",
+        readonly=True,
+        related="move_id.merged_qty_uom_info",
     )
-
-    @api.depends(
-        "move_id.sale_line_id.secondary_uom_qty",
-        "move_id.sale_line_id.secondary_uom_id.name"
-    )
-    def _compute_merged_qty_uom_info(self):
-        for line in self:
-            line.merged_qty_uom_info = "%s %s" % (
-                formatLang(
-                    self.env, line.move_id.sale_line_id.secondary_uom_qty),
-                line.move_id.sale_line_id.secondary_uom_id.name or ''
-            )
