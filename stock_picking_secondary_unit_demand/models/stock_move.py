6ff471a211c19691d6582877e129e276aabd1864
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2024-05-30 15:31:23 -0400

    TA#61866 [IMP] stock_picking_secondary_unit_demand

--- stock_picking_secondary_unit_demand/models/stock_move.py
+++ stock_picking_secondary_unit_demand/models/stock_move.py
@@ -50,13 +50,14 @@ class StockMove(models.Model):
         stock_secondary_uom_id = self.product_id.stock_secondary_uom_id
         if not stock_secondary_uom_id:
             demand_qty = 0.0
-        factor = self._get_factor_line()
+        factor = (self._get_factor_line() 
+                  or self.product_id.stock_secondary_uom_id.factor or 1.0)
         precision_rounding = (
             self.product_id.stock_secondary_uom_id.uom_id.rounding
             or self.product_uom.rounding or 0.01
         )
         demand_qty = float_round(
-            self.product_uom_qty / (factor or 1.0),
+            self.product_uom_qty / factor,
             precision_rounding=precision_rounding,
         )
         return demand_qty, stock_secondary_uom_id
