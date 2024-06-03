ff2510ab2af8ade924c3c3a5859cbe219add077e
majouda <majdaelmariouli@gmail.com>
2024-06-03 16:51:25 -0400

    TA#66190 [IMP] product_category_safe_change: Code Quality Improvment

--- product_category_safe_change/__manifest__.py
+++ product_category_safe_change/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Category Safe Change",
-    "version": "1.0.3",
+    "version": "1.0.4",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
