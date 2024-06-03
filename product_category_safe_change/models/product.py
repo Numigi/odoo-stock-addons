6df7550a156ee5b6a10540258a7096dc65f541ad
majouda <majdaelmariouli@gmail.com>
2024-06-03 16:51:25 -0400

    TA#66190 [IMP] product_category_safe_change: Code Quality Improvment

--- product_category_safe_change/models/product.py
+++ product_category_safe_change/models/product.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     _inherit = "product.product"
 
     def _check_category_stock_move(self):
-        existing_move_lines = self.env['stock.move.line'].search([
+        existing_move_lines = self.env['stock.move.line'].sudo().search([
             ('product_id', 'in', self.ids)
         ])
         if existing_move_lines:
@@ -17,7 +17,7 @@ class Product(models.Model):
                 _("You cannot modify the category of a Product with Stock Moves."))
 
     def write(self, vals):
-        if 'categ_id' in vals:
+        if self.type != "service" and "categ_id" in vals:
             self._check_category_stock_move()
         return super(Product, self).write(vals)
 
@@ -34,6 +34,6 @@ class ProductTemplate(models.Model):
                 _("You cannot modify the category of a Product with Stock Moves."))
 
     def write(self, vals):
-        if 'categ_id' in vals:
+        if self.type != "service" and "categ_id" in vals:
             self._check_category_stock_move()
         return super(ProductTemplate, self).write(vals)
