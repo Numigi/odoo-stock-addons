badb082342625a4ee87a1ac58637dfe66a7e59ff
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2024-06-27 07:35:22 +0300

    TA#66875 [16.0][MIG] stock_quant_by_category

--- Dockerfile
+++ Dockerfile
@@ -18,5 +18,7 @@ RUN gitoo install-all --conf_file /gitoo.yml --destination "${THIRD_PARTY_ADDONS
 
 USER odoo
 
+COPY stock_quant_by_category /mnt/extra-addons/stock_quant_by_category
+
 COPY .docker_files/main /mnt/extra-addons/main
 COPY .docker_files/odoo.conf /etc/odoo
---
+++ stock_quant_by_category/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Quant by Category",
+    "version": "16.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "AGPL-3",
+    "category": "Stock",
+    "depends": ["stock"],
+    "summary": "Adds product category field available on inventory report.",
+    "data": [
+        "views/stock_quant_views.xml",
+    ],
+    "installable": True,
+}
