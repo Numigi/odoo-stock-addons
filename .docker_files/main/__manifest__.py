08f7988bb1bd9f5e4a226bfd77cf7ddf537ba929
Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-09-30 09:24:24 +0300

    TA#68633 [ADD] stock_picking_delivery_carrier_required : unit test
    TA#68633 [ADD] stock_picking_delivery_carrier_required : unit test
    
    Update test_stock_picking.py
    
    Update test_stock_picking.py
    
    Update test_stock_picking.py
    
    Update test_stock_picking.py

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -29,7 +29,9 @@
         "stock_orderpoint_secondary_unit",
         "stock_orderpoint_procurement_date",
         "stock_picking_barcode",
-        "stock_picking_delivery_carrier_required",
+        # "stock_picking_delivery_carrier_required", TA#68633
+        # in conflict with OCA/OCA/stock-logistics-warehouse module: stock_secondary_unit
+        # loaded from Numigi/odoo-stock-addons module: stock_quant_secondary_unit
         "stock_picking_groupby_parent_affiliate",
         "stock_picking_responsible_editable",
         "stock_picking_secondary_unit_demand",
---
+++ stock_picking_delivery_carrier_required/tests/test_stock_picking.py
@@ -0,0 +1,55 @@
+# Copyright 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests import Form, SavepointCase, tagged
+
+
+@tagged("-at_install", "post_install")
+class TestStockPickingDelivery(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        # Only if stock_picking_delivery_carrier_required is installed on system
+        if not cls.env['ir.module.module'].search(
+            [
+                ('name', '=', 'stock_picking_delivery_carrier_required'),
+                ('state', '=', 'installed'),
+            ],
+            limit=1,
+        ):
+            cls.skipTest(
+                cls, "Module stock_picking_delivery_carrier_required is not installed"
+            )
+
+        cls.product_delivery_normal = cls.env['product.product'].create(
+            {
+                'name': 'Normal Delivery Charges',
+                'type': 'service',
+                'list_price': 10.0,
+                'categ_id': cls.env.ref('delivery.product_category_deliveries').id,
+            }
+        )
+        cls.normal_delivery = cls.env['delivery.carrier'].create(
+            {
+                'name': 'Normal Delivery Charges',
+                'fixed_price': 10,
+                'delivery_type': 'fixed',
+                'product_id': cls.product_delivery_normal.id,
+            }
+        )
+
+    def test_shipping_info_required(self):
+        # Check if carrier_id and carrier_tracking_ref are required
+        delivery_form = Form(self.env["stock.picking"])
+        delivery_form.picking_type_id = self.env.ref("stock.picking_type_out")
+        delivery_form.location_id = self.env.ref("stock.stock_location_stock")
+        delivery_form.location_dest_id = self.env.ref("stock.stock_location_customers")
+
+        with self.assertRaises(AssertionError):
+            delivery_form.save()
+
+        delivery_form.carrier_id = self.normal_delivery
+        delivery_form.carrier_tracking_ref = "123456"
+
+        delivery_form.save()
