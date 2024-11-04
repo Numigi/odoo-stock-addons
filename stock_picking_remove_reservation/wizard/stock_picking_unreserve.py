19434809e48c393be964e1eece0c86b7e53303ab
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-11-04 22:29:08 +0100

    TA#70700[14.0][ADD] stock_picking_remove_reservation

--- stock_picking_remove_reservation/wizard/stock_picking_unreserve.py
+++ stock_picking_remove_reservation/wizard/stock_picking_unreserve.py
@@ -1,7 +1,7 @@
 # © 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import models
 
 
 class StockPickingUnreserve(models.TransientModel):
