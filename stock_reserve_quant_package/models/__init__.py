cbbbd94a2b81ccfdbf54a2ea6b27e84514df84f0
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-11-04 22:24:09 +0100

    TA#70700[14.0][ADD] stock_picking_remove_reservation

--- stock_reserve_quant_package/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# © 2022 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import stock_move
-from . import stock_picking
-from . import stock_quant
