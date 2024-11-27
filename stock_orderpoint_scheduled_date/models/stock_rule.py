a45a7235fe01433fe2925371176a393414f66b8c
majouda <majdaelmariouli@gmail.com>
2024-11-27 18:52:45 -0500

    TA#70572 [14.0][FIX] stock_orderpoint_scheduled_date: get lead days from scheduled date

---
+++ stock_orderpoint_scheduled_date/models/stock_rule.py
@@ -0,0 +1,28 @@
+# © 2024 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, _
+
+
+class StockRule(models.Model):
+    _inherit = "stock.rule"
+
+    def _get_lead_days(self, product):
+        """Add the delay from the manually entered scheduled date in the replenishment
+        order to the cumulative delay and cumulative description.
+        """
+        delay, delay_description = super()._get_lead_days(product)
+        scheduled_date = self.env.context.get("scheduled_date")
+        bypass_delay_description = self.env.context.get(
+            "bypass_delay_description", False
+        )
+        if scheduled_date:
+            # Convert the scheduled date from string to UTC datetime
+            manual_delay = (scheduled_date - fields.Datetime.now()).days
+            delay += manual_delay
+            if not bypass_delay_description:
+                delay_description += (
+                    "<tr><td>%s</td><td class='text-right'>+ %d %s</td></tr>"
+                    % (_("Manual Lead Time"), manual_delay, _("day(s)"))
+                )
+        return delay, delay_description
--- stock_orderpoint_scheduled_date/models/stock_warehouse_orderpoint.py
+++ stock_orderpoint_scheduled_date/models/stock_warehouse_orderpoint.py
@@ -2,28 +2,50 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models, fields, _
+from odoo.exceptions import ValidationError
 
 
 class stockWarehouseOrderpoint(models.Model):
     _inherit = "stock.warehouse.orderpoint"
 
-    scheduled_date = fields.Date("Scheduled Date")
-    previous_scheduled_date = fields.Date(
-        "Previous Scheduled Date", default=fields.Date.today()
+    scheduled_date = fields.Datetime(
+        "Scheduled Date", help="The date when this replenishment should be scheduled."
     )
 
-    @api.depends('rule_ids', 'product_id.seller_ids',
-                 'product_id.seller_ids.delay', 'scheduled_date')
+    @api.depends(
+        "rule_ids",
+        "product_id.seller_ids",
+        "product_id.seller_ids.delay",
+        "scheduled_date",
+    )
     def _compute_lead_days(self):
-        for orderpoint in self.with_context(bypass_delay_description=True):
-            if orderpoint.scheduled_date:
-                orderpoint.lead_days_date = orderpoint.scheduled_date
-            elif orderpoint.previous_scheduled_date:
-                orderpoint.lead_days_date = orderpoint.previous_scheduled_date
-            else:
-                super(stockWarehouseOrderpoint, orderpoint)._compute_lead_days()
+        for orderpoint in self:
+            # Pass scheduled_date via context for lead time computation
+            super(
+                stockWarehouseOrderpoint,
+                orderpoint.with_context(scheduled_date=orderpoint.scheduled_date),
+            )._compute_lead_days()
 
-    def open_set_schedule_date_wizard(self):
+    @api.depends(
+        "rule_ids",
+        "product_id.seller_ids",
+        "product_id.seller_ids.delay",
+        "scheduled_date",
+    )
+    def _compute_json_popover(self):
+        for orderpoint in self:
+            # Pass scheduled_date and delay description context
+            super(
+                stockWarehouseOrderpoint,
+                orderpoint.with_context(
+                    scheduled_date=orderpoint.scheduled_date,
+                    bypass_delay_description=False,
+                ),
+            )._compute_json_popover()
+
+    @api.model
+    def action_open_set_schedule_date_wizard(self):
+        """Open a wizard to set the scheduled date."""
         wizard = self.env['stock.warehouse.orderpoint.schedule.date'].create({})
         wizard.orderpoint_ids = self
         action = wizard.get_formview_action()
@@ -31,9 +53,34 @@ class stockWarehouseOrderpoint(models.Model):
         action['name'] = _('Set Schedule Date')
         return action
 
-    def action_replenish(self):
-        result = super(stockWarehouseOrderpoint, self).action_replenish()
-        self.write(
-            {"previous_scheduled_date": self.scheduled_date, "scheduled_date": False}
-        )
-        return result
+    @api.model
+    def action_open_orderpoints(self):
+        """Override to pass scheduled_date in the context."""
+        return super(
+            stockWarehouseOrderpoint,
+            self.with_context(scheduled_date=self.scheduled_date),
+        ).action_open_orderpoints()
+
+    def _procure_orderpoint_confirm(
+        self, use_new_cursor=False, company_id=None, raise_user_error=True
+    ):
+        """Ensure orderpoints with different scheduled dates aren't processed together."""
+        scheduled_dates = set(self.mapped("scheduled_date"))
+        if len(scheduled_dates) > 1:
+            raise ValidationError(
+                _("Please select lines with the same scheduled date.")
+            )
+        return super(
+            stockWarehouseOrderpoint,
+            self.with_context(scheduled_date=self[0].scheduled_date),
+        )._procure_orderpoint_confirm(use_new_cursor, company_id, raise_user_error)
+
+    def _prepare_procurement_values(self, date=False, group=False):
+        """Prepare procurement values with consideration for the scheduled date."""
+        values = super()._prepare_procurement_values(date=date, group=group)
+        if self.scheduled_date:
+            values["date_planned"] = self.scheduled_date
+            values["date_deadline"] = self.scheduled_date
+            # Reset scheduled_date after processing
+            self.scheduled_date = False
+        return values
