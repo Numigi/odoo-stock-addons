69d124a1a0f579d166a741ce98b60a6d99f03505
Lanto Razafindrabe <124650562+lanto-razafindrabe@users.noreply.github.com>
2025-06-20 12:33:33 +0300

    TA#77932 [12.0][ADD] stock_account_fifo_fix_valuation (#213)
    Co-authored-by: Abdellatif BENZBIRIA <a.benzbiria@gmail.com>

---
+++ stock_account_fifo_fix_valuation/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2025 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Account FIFO Fix Valuation",
+    "version": "12.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "AGPL-3",
+    "category": "stock",
+    "depends": ["stock_account"],
+    "summary": """
+        This module fixes picking return stock valuation,
+        especially stock valuation for FIFO and valuation is in automated mode.
+    """,
+    "data": [],
+    "installable": True,
+}
