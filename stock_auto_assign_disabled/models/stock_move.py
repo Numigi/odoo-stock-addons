893e541696a99c55d479db829c988ce7ddba694b
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-02-25 17:15:43 +0000

    [IMP] stock_auto_assign_disable

--- stock_auto_assign_disabled/models/stock_move.py
+++ stock_auto_assign_disabled/models/stock_move.py
@@ -2,9 +2,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, api
-import logging
-
-_logger = logging.getLogger(__name__)
 
 
 class StockMove(models.Model):
@@ -13,7 +10,10 @@ class StockMove(models.Model):
     def _should_process_auto_reservation(self):
         return self.product_id.tracking not in ["serial", "lot"]
 
-    def _get_disabled_reservation_context(self):
+    def _filter_moves_for_auto_assign(self):
+        """
+        Filtre les mouvements ou applique le contexte d'interdiction.
+        """
         mode = self.env["ir.config_parameter"].sudo().get_param(
             "stock_auto_assign_disabled.config", "off"
         )
@@ -22,40 +22,51 @@ class StockMove(models.Model):
 
         if is_scheduler or is_wizard:
             if mode == "all":
-                return {'disable_reservation': True}
+                return self.with_context(disable_reservation=True)
+            elif mode == "serial_lot":
+                # On filtre et on retire les articles tracés par lot/série (les tests refonctionneront)
+                return self.filtered(lambda x: x._should_process_auto_reservation())
 
-        return {}
-
-    def _filter_moves_for_auto_assign(self):
-        ctx = self._get_disabled_reservation_context()
-        if ctx.get('disable_reservation'):
-            return self.with_context(**ctx)
         return self
 
     @api.multi
     def _action_confirm(self, merge=True, merge_into=False):
-        ctx = self._get_disabled_reservation_context()
-        if ctx.get('disable_reservation'):
-            self = self.with_context(**ctx)
+        # Pour contrer la réservation immédiate au moment de la confirmation
+        mode = self.env["ir.config_parameter"].sudo().get_param(
+            "stock_auto_assign_disabled.config", "off"
+        )
+        is_scheduler = self._context.get("stock_auto_assign_disable")
+        is_wizard = self._context.get("disable_reservation")
+
+        if (is_scheduler or is_wizard) and mode == "all":
+            self = self.with_context(disable_reservation=True)
 
         return super(StockMove, self)._action_confirm(merge=merge, merge_into=merge_into)
 
     @api.multi
     def _action_assign(self):
-        ctx = self._get_disabled_reservation_context()
-        if ctx.get('disable_reservation') or self._context.get('disable_reservation'):
-            _logger.info("STOCK_DISABLED: _action_assign bloqué par le contexte")
-            # On stoppe l'action ici
-            return True
+        # On applique le filtre (qui gère 'all' et 'serial_lot')
+        moves_to_assign = self._filter_moves_for_auto_assign()
 
-        return super(StockMove, self)._action_assign()
+        if moves_to_assign._context.get('disable_reservation'):
+            return True  # On simule le succès sans appeler le super()
+
+        return super(StockMove, moves_to_assign)._action_assign()
 
     def _update_reserved_quantity(self, need, available_quantity, location_id,
                                   lot_id=None, package_id=None, owner_id=None, strict=True):
+        """
+        Protection de bas niveau (La porte de sortie).
+        """
+        mode = self.env["ir.config_parameter"].sudo().get_param("stock_auto_assign_disabled.config", "off")
+        is_scheduler = self._context.get("stock_auto_assign_disable")
+        is_wizard = self._context.get("disable_reservation")
 
-        ctx = self._get_disabled_reservation_context()
-        if ctx.get('disable_reservation') or self._context.get('disable_reservation'):
-            return 0.0
+        if is_scheduler or is_wizard:
+            if mode == "all":
+                return 0.0
+            elif mode == "serial_lot" and not self._should_process_auto_reservation():
+                return 0.0
 
         return super(StockMove, self)._update_reserved_quantity(
             need, available_quantity, location_id, lot_id, package_id, owner_id, strict
