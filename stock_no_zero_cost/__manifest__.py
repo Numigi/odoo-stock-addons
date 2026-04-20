799439f1de7f3017b4a4fe941d2d9f4d43362abf
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-04-20 11:21:28 +0000

    TA#83176 [14.0][IMP] : stock_no_zero_cost : change usererror message

--- stock_no_zero_cost/__manifest__.py
+++ stock_no_zero_cost/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Stock Zero Cost Protection',
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.0.1',
     'category': 'Inventory/Inventory',
     'summary': 'Prevent zero cost valuation on stock moves with a security bypass',
     "maintainer": "numigi",
