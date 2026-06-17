7e4ebd432722acd79dc441738edfc674d11bb4a4
Quentin Lavallée-Bourdeau <quentin.lavalleeb@gmail.com>
2026-06-17 16:59:37 -0400

    TA#83757 [14.0][IMP] delivery_custom_notification : bump version to 1.2.0

--- delivery_custom_notification/__manifest__.py
+++ delivery_custom_notification/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Delivery Custom Notification",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.2.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://numigi.com/r/home",
